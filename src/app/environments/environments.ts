73f0a683d12effebf3482c1cc684e3b851784440
Rodrigo-T-Matioli <57012803+Rodrigo-T-Matioli@users.noreply.github.com>
2024-05-25 14:31:56 -0300

    Enviroments

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  api: 'https://api-nestjs-eficacia-ogdznc471-rodrigotmatiolis-projects.vercel.app'
+  api: 'https://api-nestjs-eficacia-fyfjahhzo-rodrigotmatiolis-projects.vercel.app'
   // api: 'http://localhost:3000'
 }
